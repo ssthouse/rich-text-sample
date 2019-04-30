45464626c89b0a42b31f3401b36f8496dafe2dda
ssthouse <ssthouse@163.com>
2019-04-30 20:09:08 +0800

    实现最基本浏览器自带富文本框架

--- src/iframe/iframe.jsx
+++ src/iframe/iframe.jsx
@@ -1,12 +1,30 @@
 import React, {Component} from 'react';
+import iframeTemplate from './template.html';
 
-export  default class IFrame extends Component{
+export default class IFrame extends Component {
 
+
+  initIframe() {
+    const content = 'test content';
+    const iframe = document.getElementById('iframe')
+    iframe.src = "data:text/html;charset=utf-8," + iframeTemplate.replace('${content}', content);
+  }
+
+
+  componentDidMount() {
+    this.initIframe();
+  }
 
   render() {
     return (
       <div>
-        iframe
+        <div className="tool-bar">
+          <span>this is the toolbar</span>
+        </div>
+        <div className="editor-content">
+          <iframe id="iframe"
+                  src="javascript:void(0);"/>
+        </div>
       </div>
     );
   }
