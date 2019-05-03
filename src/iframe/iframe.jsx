814c0ecd345c9b4cecd6fe65c99f8d2f6cb8b477
ssthouse <ssthouse@163.com>
2019-05-03 11:50:56 +0800

    完成保存加载逻辑

--- src/iframe/iframe.jsx
+++ src/iframe/iframe.jsx
@@ -1,17 +1,26 @@
 import React, {Component} from 'react';
 import iframeTemplate from './template.html';
 import Toolbar from './toolbar/toolbar.jsx';
+import {Button} from 'antd'
 
 export default class IFrame extends Component {
 
   constructor(options) {
     super(options);
+    this.initWindowListener();
     this.onToolbarAction = this.onToolbarAction.bind(this);
+  }
+
+  initWindowListener() {
+    window.addEventListener('message', event => {
+      const content = event.data.content;
+      localStorage.setItem('iframeContent', content);
+    })
   }
 
   initIframe() {
     const iframe = document.getElementById('iframe');
-    iframe.src = "data:text/html;charset=utf-8," + iframeTemplate;
+    iframe.src = "data:text/html;charset=utf-8," + iframeTemplate.replace('$body', '');
   }
 
   componentDidMount() {
@@ -20,6 +29,17 @@ export default class IFrame extends Component {
 
   onToolbarAction(cmdEvent) {
     document.getElementById('iframe').contentWindow.postMessage(cmdEvent, '*');
+  }
+
+  onClickSave() {
+    document.getElementById('iframe').contentWindow.postMessage({
+      type: 'getContent'
+    }, '*');
+  }
+
+  onClickLoadStorage() {
+    const content = localStorage.getItem('iframeContent') || '';
+    document.getElementById('iframe').src = "data:text/html;charset=utf-8," + iframeTemplate.replace('$body', content);
   }
 
   render() {
@@ -32,6 +52,8 @@ export default class IFrame extends Component {
           <iframe id="iframe"
                   src="javascript:void(0);"/>
         </div>
+        <Button onClick={this.onClickSave}>Save</Button>
+        <Button onClick={this.onClickLoadStorage}>Load last saved content</Button>
       </div>
     );
   }
